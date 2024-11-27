33b2e6dc1fa89890cf21eca39d79fb934aedf47d
anggitaac <147733412+anggitaac@users.noreply.github.com>
2024-11-28 04:03:02 +0700

    Update ikan.py

--- ikan.py
+++ ikan.py
@@ -55,4 +55,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    # Menjalankan aplikasi pada port yang ditentukan di Railway atau pada 5000 sebagai default
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
